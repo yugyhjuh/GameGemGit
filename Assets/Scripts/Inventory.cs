b2cc3dd2177fd89555665d344fa6d961e0d926af
yugyhjuh <68283680+yugyhjuh@users.noreply.github.com>
2024-10-17 01:07:34 +0800

    updated assets

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,6 +7,9 @@ public class Inventory : MonoBehaviour
     public List<Item> items = new List<Item>();  // List to hold items
     public int maxInventorySize = 10;  // Maximum items allowed in inventory
 
+    public GameObject incompleteRocket;
+    public GameObject completeRocket;
+
     // Adds an item to the inventory
     public bool AddItem(Item newItem)
     {
@@ -36,6 +39,25 @@ public class Inventory : MonoBehaviour
     void Update() {
         if (Input.GetKeyDown(KeyCode.I)) {
             ShowInventory();
+        }
+
+        if (items.Count <= 2)
+        {
+            //Debug.Log("Rocket Unable to Board: Missing Parts");
+            completeRocket.SetActive(false);  // Disable gameobject1
+            incompleteRocket.SetActive(true);   // Enable gameobject2
+        }
+        else if (items.Count >= 3)
+        {
+            //Debug.Log("Rocket Launching... Failure...Self-Destructing");
+            completeRocket.SetActive(true);  // Disable gameobject1
+            incompleteRocket.SetActive(false);   // Enable gameobject2
+        }
+        else if (items.Count >= 5)
+        {
+            //Debug.Log("Rocket Launching... TO EARTH!");
+            completeRocket.SetActive(true);  // Disable gameobject1
+            incompleteRocket.SetActive(false);   // Enable gameobject2
         }
     }
     // Displays inventory contents in the console
