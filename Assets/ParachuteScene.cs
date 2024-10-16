2dcbec15d76254069ca5b69d35a8a84ee00fa3ed
yugyhjuh <68283680+yugyhjuh@users.noreply.github.com>
2024-10-17 02:31:00 +0800

    more asset stuff + win screen

---
+++ Assets/ParachuteScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParachuteScene : MonoBehaviour
+{
+    private bool playerIsCloser = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsCloser)
+        {
+            SceneManager.LoadSceneAsync(3);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsCloser = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsCloser = false;
+        }
+    }
+}
