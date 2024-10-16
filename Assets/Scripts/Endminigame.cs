dfb4804a7a7bab581210975d11b78d2cf0dcb198
demoncharge <168311406+demoncharge@users.noreply.github.com>
2024-10-17 02:31:37 +0800

    elaop[fd

--- Assets/Scripts/Endminigame.cs
+++ Assets/Scripts/Endminigame.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Endminigame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool miniplayerIsClose = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(miniplayerIsClose == true)
+        {
+            SceneManager.LoadSceneAsync(4);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            miniplayerIsClose = true;
+        }
     }
 }
