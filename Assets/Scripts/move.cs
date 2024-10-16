896a0a8c58716f434474862af6dd627ccfcba3af
Alvin Ng <alvinnws4546@gmail.com>
2024-10-16 17:45:50 +0800

    Comments

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    // Health Bar
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
 
+    // Movement
     private Rigidbody2D rb;
     private Vector2 playerVelocity;
     public float playerSpeed = 5.0f;
-    public float jumpHeight = 5.0f;  // Jump force (use higher value for 2D jumping)
-    public float gravityValue = -9.81f;  // Default gravity for 2D physics
+    public float jumpHeight = 5.0f; 
+    public float gravityValue = -9.81f;
 
+    // Ground Checking
     private bool isGrounded;
     public Transform groundCheck;  // Reference to the empty GameObject below player's feet
     public float groundCheckRadius = 0.2f;  // Size of the ground check circle
     public LayerMask groundLayer;  // Ground layer for detection
 
+    // Teleporter
     private GameObject currentTeleporter;
 
     void Start()
@@ -32,24 +36,25 @@ public class move : MonoBehaviour
 
     void Update()
     {
+        // Health Bar
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(20);
         }
 
-        // Check if player is grounded by using Physics2D.OverlapCircle
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         // Horizontal movement
         float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * playerSpeed, rb.velocity.y);  // Set horizontal velocity
+        rb.velocity = new Vector2(moveInput * playerSpeed, rb.velocity.y);
 
         // Jumping logic
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         if (Input.GetAxis("Vertical") > 0 && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Sqrt(jumpHeight * -2.0f * gravityValue));
         }
 
+        // Teleporter
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (currentTeleporter != null)
